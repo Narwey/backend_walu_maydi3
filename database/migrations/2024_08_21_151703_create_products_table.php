9d9afcc94ac0cd540311e7c4dd1a8cd588eb1892
anouar ben mbarek <benmbark.anouar@gmail.com>
2024-08-21 16:50:40 +0100

    Add new models for admin, seller, customer, product, order, payment, and order items

---
+++ database/migrations/2024_08_21_151703_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Customer;
+use App\Models\Seller;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Customer::class);
+            $table->foreignIdFor(Seller::class);
+            $table->decimal('total-amount', 5, 2)->nullable()->default(123.45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
